e48fc4e97e819adc6b3485fb34c2410aef30a71b
Camilo Calderón Rocha <46984337+camilocallderon@users.noreply.github.com>
2020-08-09 16:08:05 -0500

    Page Product Lista para firebase
    Se crea un modelo de un posible porducto para hacer pruebas con firebase

--- Clapp/lib/src/User/pages/producto_page.dart
+++ Clapp/lib/src/User/pages/producto_page.dart
@@ -1,6 +1,17 @@
+import 'package:Clapp/src/User/models/producto_models.dart';
 import 'package:flutter/material.dart';
+import 'package:Clapp/src/User/utils/utils.dart' as utils;
 
-class ProductoPage extends StatelessWidget {
+class ProductoPage extends StatefulWidget {
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+}
+
+class _ProductoPageState extends State<ProductoPage> {
+  final formKey = GlobalKey<FormState>();
+
+  ProductoModel producto = new ProductoModel();
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -28,10 +39,13 @@ class ProductoPage extends StatelessWidget {
           child: Container(
             padding: EdgeInsets.all(15.0),
             child: Form(
+              key: formKey,
               child: Column(
                 children: <Widget>[
                   _crearNombre(),
                   _crearPrecio(),
+                  _crearDisponible(),
+                  _crearBoton(),
                 ],
               ),
             ),
@@ -43,19 +57,69 @@ class ProductoPage extends StatelessWidget {
 
   Widget _crearNombre() {
     return TextFormField(
+      initialValue: producto.titulo,
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
         labelText: 'Producto',
       ),
+      onSaved: (value) => producto.titulo = value,
+      validator: (value) {
+        if (value.length < 3) {
+          return 'Ingrese el nombre del producto correctamente';
+        } else {
+          return null;
+        }
+      },
     );
   }
 
   Widget _crearPrecio() {
     return TextFormField(
-      keyboardType: TextInputType.number,
+      initialValue: producto.valor.toString(),
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
       decoration: InputDecoration(
         labelText: 'Precio',
       ),
+      onSaved: (value) => producto.valor = double.parse(value),
+      validator: (value) {
+        if (utils.isNumeric(value)) {
+          return null;
+        } else {
+          return 'Solo numeros';
+        }
+      },
     );
+  }
+
+  Widget _crearDisponible() {
+    return SwitchListTile(
+      value: producto.disponible,
+      title: Text('Disponible'),
+      activeColor: Color.fromRGBO(153, 255, 204, 1.0),
+      onChanged: (value) => setState(() {
+        producto.disponible = value;
+      }),
+    );
+  }
+
+  Widget _crearBoton() {
+    return RaisedButton.icon(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      color: Color.fromRGBO(89, 122, 121, 1.0),
+      textColor: Colors.white,
+      label: Text('Guardar'),
+      icon: Icon(Icons.save),
+      onPressed: _submit,
+    );
+  }
+
+  void _submit() {
+    if (!formKey.currentState.validate()) return;
+
+    formKey.currentState.save();
+
+    print('Todo Ok');
   }
 }
