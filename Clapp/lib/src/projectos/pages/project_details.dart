15e527e34cf47fa142ed4aab93f5a037642dccbe
sebastianpedraza2 <46971682+sebastianpedraza2@users.noreply.github.com>
2020-09-17 18:43:54 -0500

    Cambio diseño menu proyectos

--- Clapp/lib/src/projectos/pages/project_details.dart
+++ Clapp/lib/src/projectos/pages/project_details.dart
@@ -1,6 +1,7 @@
 import 'package:Clapp/src/Contract/pages/ver_contratos.dart';
 import 'package:Clapp/src/User/models/user_model.dart';
 import 'package:Clapp/src/projectos/model/project_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProjectDetails extends StatefulWidget {
@@ -10,126 +11,212 @@ class ProjectDetails extends StatefulWidget {
   _ProjectDetails createState() => _ProjectDetails();
 }
 
-class _ProjectDetails extends State<ProjectDetails>{
+class _ProjectDetails extends State<ProjectDetails> {
   @override
-
   Widget build(BuildContext context) {
-    ProjectModel proyecto= ModalRoute.of(context).settings.arguments;
-    final List<Widget> _screens = [
-
-    ];
+    ProjectModel proyecto = ModalRoute.of(context).settings.arguments;
+    final List<Widget> _screens = [];
     return Container(
       child: Scaffold(
         appBar: AppBar(
-          title: Text(proyecto.proyectName,
+          centerTitle: true,
+          title: Text('Proyectos',
               style: TextStyle(fontSize: 20.0, fontFamily: "Raleway")),
-          ),
-        body: Column(
+        ),
+        body: Container(
+    width: MediaQuery.of(context).size.width,
+    height: MediaQuery.of(context).size.height - 30.0,
+    padding: EdgeInsets.only(
+    right: 15.0,
+    left: 15.0,
+    ),
+    child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            _descriptionCard(proyecto.description),
-            _contactCard(proyecto.contacto),
-            RaisedButton.icon(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
+
+            Container(
+                padding: EdgeInsets.only(top: 5.0),
+                child: Center(child: Text('Proyecto - ${proyecto.proyectName}',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Color.fromRGBO(115, 115, 115, 1.0),
+                        fontSize: 17.5,
+                        fontFamily: "Raleway",
+                        fontWeight: FontWeight.bold)))
+            ),
+            SizedBox(height: 18,),
+
+            Container(
+              padding: EdgeInsets.only(left: 10,bottom: 4),
+              child: Text(
+                  'Descripcion de la obra',
+                  style: TextStyle(
+                      color: Color.fromRGBO(0, 51, 51, 0.8),
+                      fontSize: 20.5,
+                      fontFamily: "Raleway",
+                      fontWeight: FontWeight.bold)
               ),
-              label: Text('Contratos abiertos',
+            ),
+            _descriptionCard(proyecto.description),
+            //_contactCard(proyecto.contacto),
+            Padding(
+              padding: EdgeInsets.only(top: 19, ),
+              child: RaisedButton(
+              //splashColor: Colors.green,
+              padding:
+              EdgeInsets.only(top: 13, bottom: 13, left: 10, right: 10),
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                    color: Color.fromRGBO(0, 51, 51, 0.8), width: 1.2),
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Text('Posiciones Disponibles',
                   textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 20.0, fontFamily: "Raleway")),
-              textColor: Colors.white,
-              icon: Icon(Icons.description, color: Colors.white),
-              color: Color.fromRGBO(89, 122, 121, 1.0),
-              padding: EdgeInsets.symmetric(horizontal: 45.5),
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: "Raleway",
+                      color: Color.fromRGBO(0, 51, 51, 0.8),
+                      fontWeight: FontWeight.bold)),
+              textColor: Color.fromRGBO(0, 51, 51, 0.8),
+              color: Colors.white,
               onPressed: () {
                 Navigator.pushNamed(context, 'see_contracts',
                     arguments: proyecto);
               },
+            ),),
+
+      SizedBox(
+
+        width: 200,
+        child: Padding(
+          padding: EdgeInsets.only(top: 19,),
+          child: RaisedButton(
+            //splashColor: Colors.green,
+            padding:
+            EdgeInsets.only(top: 13, bottom: 13, left: 10, right: 10),
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                  color: Color.fromRGBO(0, 51, 51, 0.8), width: 1.2),
+              borderRadius: BorderRadius.circular(15.0),
             ),
-            RaisedButton.icon(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              label: Text('Contratados',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 20.0, fontFamily: "Raleway")),
-              textColor: Colors.white,
-              icon: Icon(Icons.accessibility, color: Colors.white),
-              color: Color.fromRGBO(89, 122, 121, 1.0),
-              padding: EdgeInsets.symmetric(horizontal: 72.5),
-              onPressed: () {
-                Navigator.pushNamed(context, 'collaborators',
-                    arguments: proyecto);
-              },
+            child: Text('Colaboradores',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: "Raleway",
+                    color: Color.fromRGBO(0, 51, 51, 0.8),
+                    fontWeight: FontWeight.bold)),
+            textColor: Color.fromRGBO(0, 51, 51, 0.8),
+            color: Colors.white,
+            onPressed: () {
+              Navigator.pushNamed(context, 'collaborators',
+                  arguments: proyecto);
+            },
+          ),),
+      ),
+
+      SizedBox(
+        width: 200,
+        child: Padding(
+          padding: EdgeInsets.only(top: 19, ),
+          child: RaisedButton(
+            //splashColor: Colors.green,
+            padding:
+            EdgeInsets.only(top: 13, bottom: 13, left: 10, right: 10),
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                  color: Color.fromRGBO(0, 51, 51, 0.8), width: 1.2),
+              borderRadius: BorderRadius.circular(15.0),
             ),
-            RaisedButton.icon(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              label: Text('Financiación',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 20.0, fontFamily: "Raleway")),
-              textColor: Colors.white,
-              icon: Icon(Icons.attach_money, color: Colors.white),
-              color: Color.fromRGBO(89, 122, 121, 1.0),
-              padding: EdgeInsets.symmetric(horizontal: 70.5),
-              onPressed: () {
-                //Navigator.push();
-              },
+            child: Text('Finanzas',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: "Raleway",
+                    color: Color.fromRGBO(0, 51, 51, 0.8),
+                    fontWeight: FontWeight.bold)),
+            textColor: Color.fromRGBO(0, 51, 51, 0.8),
+            color: Colors.white,
+            onPressed: () {},
+          ),),
+      ),
+
+
+
+      SizedBox(
+        width: 200,
+        child:  Padding(
+          padding: EdgeInsets.only(top: 19,),
+          child: RaisedButton(
+            //splashColor: Colors.green,
+            padding:
+            EdgeInsets.only(top: 13, bottom: 13, left: 10, right: 10),
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                  color: Color.fromRGBO(0, 51, 51, 0.8), width: 1.2),
+              borderRadius: BorderRadius.circular(15.0),
             ),
-            RaisedButton.icon(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              label: Text('Servicios adquiridos',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 20.0, fontFamily: "Raleway")),
-              textColor: Colors.white,
-              icon: Icon(Icons.assignment_turned_in, color: Colors.white),
-              color: Color.fromRGBO(89, 122, 121, 1.0),
-              padding: EdgeInsets.symmetric(horizontal: 35.5),
-              onPressed: () {
-                //Navigator.push();
-              },
-            ),
+            child: Text('Contacto',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: "Raleway",
+                    color: Color.fromRGBO(0, 51, 51, 0.8),
+                    fontWeight: FontWeight.bold)),
+            textColor: Color.fromRGBO(0, 51, 51, 0.8),
+            color: Colors.white,
+            onPressed: () {},
+          ),),
+      ),
+
           ],
-        ),
-        ),
-      );
-
-  }
-
-  Widget _descriptionCard(String description){
-    return Card(
-      elevation: 20.0,
-      shape:
-      RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-      child: Column(
-          children: <Widget>[
-      ListTile(
-      title: Text('Descripcion',
-          style: TextStyle(fontSize: 20.0, fontFamily: "Raleway")),
-      subtitle: Text(description,
-          style: TextStyle(fontSize: 12.0, fontFamily: "Raleway")),
-      )
-          ]
-      )
+        ),),
+      ),
     );
   }
 
-  Widget _contactCard(String contact){
+  Widget _descriptionCard(String description) {
+
+
+    return SizedBox(
+        width: MediaQuery.of(context).size.width -30,
+        height: 100,
+
+        child: Padding(
+          padding: EdgeInsets.only(top: 1, ),
+          child: Container(
+            
+            padding: EdgeInsets.only(top: 13, bottom: 13, left: 10, right: 10),
+
+
+            decoration: BoxDecoration(
+
+
+                borderRadius: BorderRadius.circular(15.0),
+              border: Border.all(width: 1.3, color: Color.fromRGBO(0, 51, 51, 0.8),),
+              ),
+
+            child: Text(description,
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 20.0, fontFamily: "Raleway",
+                    color: Color.fromRGBO(115, 115, 115, 1.0),
+                    fontWeight: FontWeight.bold)
+            ),
+          ),));
+  }
+
+  Widget _contactCard(String contact) {
     return Card(
         elevation: 20.0,
         shape:
-        RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-        child: Column(
-            children: <Widget>[
-              ListTile(
-                title: Text('Contacto',
-                    style: TextStyle(fontSize: 20.0, fontFamily: "Raleway")),
-                subtitle: Text(contact,
-                    style: TextStyle(fontSize: 12.0, fontFamily: "Raleway")),
-              )
-            ]
-        )
-    );
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        child: Column(children: <Widget>[
+          ListTile(
+            title: Text('Contacto',
+                style: TextStyle(fontSize: 20.0, fontFamily: "Raleway")),
+            subtitle: Text(contact,
+                style: TextStyle(fontSize: 12.0, fontFamily: "Raleway")),
+          )
+        ]));
   }
 }
