8ded9052584a27c2d86a088eaf25a63ea3afc61e
Esteban Lemus <esteban.lemus@javeriana.edu.co>
2020-09-18 10:36:57 -0500

    correccion bugs menores

--- Clapp/lib/src/services/pages/services_page.dart
+++ Clapp/lib/src/services/pages/services_page.dart
@@ -46,30 +46,7 @@ class _ServicesPages extends State<ServicesPages> {
               SizedBox(height: 10),
 
 
-              Container(
-                padding: EdgeInsets.only(right: 10.0, left: 210, bottom: 35.0),
-                child: RaisedButton(
-                  padding:
-                  EdgeInsets.only(top: 13, bottom: 13, left: 10, right: 10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Text('Nuevo servicio',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          fontFamily: "Raleway",
-                          color: Color.fromRGBO(115, 115, 115, 1.0),
-                          fontWeight: FontWeight.bold)),
-                  textColor: Colors.white,
-                  color: Color.fromRGBO(112, 252, 118, 0.8),
-                  onPressed: (){
-                    Navigator.pushNamed(context, 'new_service',
-                        arguments: usuario);
-                    },
-                ),
-              )
-
+              
             ],
           ),
         ));
