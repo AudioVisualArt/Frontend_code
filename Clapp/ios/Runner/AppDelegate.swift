d6d210a35a44ca4c488b41d07543cf76e4ecc05f
Camilo Calderón Rocha <46984337+camilocallderon@users.noreply.github.com>
2020-09-22 19:28:02 -0500

    Dependencias de iOS para Mapas

--- Clapp/ios/Runner/AppDelegate.swift
+++ Clapp/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import GoogleMaps
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,3 +13,16 @@ import Flutter
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCXag7E0pSEEIGWd2varqRopTWIxSHhqVc")
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
