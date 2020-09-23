57b764660b0bc18fb86470b07772b128f30a76fa
Camilo Calderón Rocha <46984337+camilocallderon@users.noreply.github.com>
2020-09-22 19:53:12 -0500

    Corrección de dependencias iOS

--- Clapp/ios/Runner/AppDelegate.swift
+++ Clapp/ios/Runner/AppDelegate.swift
@@ -9,17 +9,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-}
-
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyCXag7E0pSEEIGWd2varqRopTWIxSHhqVc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
