19f57c9652744c1cd66927744cab15aa8fd0770a
sebastianpedraza2 <46971682+sebastianpedraza2@users.noreply.github.com>
2020-09-29 19:07:01 -0500

    Nuevo diseño de Home page

--- Clapp/lib/src/User/pages/home_page.dart
+++ Clapp/lib/src/User/pages/home_page.dart
@@ -12,11 +12,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  ChatProvider chatprovider=new ChatProvider();
+  ChatProvider chatprovider = new ChatProvider();
   @override
   Widget build(BuildContext context) {
     UserModel usuario = ModalRoute.of(context).settings.arguments;
-   
+
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.white,
@@ -28,8 +28,7 @@ class _HomePageState extends State<HomePage> {
         userModel: usuario,
       ),
       //Rest backend Springboot
-      body: _crearBody(context, usuario)
-      ,
+      body: _crearBody(context, usuario),
     );
   }
 
@@ -39,86 +38,26 @@ class _HomePageState extends State<HomePage> {
     return Container(
       decoration: BoxDecoration(
           image: DecorationImage(
-        image: AssetImage("assets/img/Home2.jpg"),
+        image: AssetImage("assets/img/homeback2.jpg"),
         fit: BoxFit.cover,
       )),
       child: Container(
         height: MediaQuery.of(context).size.height - 30.0,
         width: size.width,
-        margin: EdgeInsets.symmetric(vertical: 10.0),
-        padding: EdgeInsets.symmetric(vertical: 25.0),
-        child: Form(
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                _mostrarFotoPerfil(),
-                SizedBox(height: 70.0),
-                SizedBox(
-                  width: 150,
-                  height: 50,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: Text('Estudio',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: "Raleway",
-                            fontWeight: FontWeight.bold)),
-                    textColor: Color.fromRGBO(115, 115, 115, 1.0),
-                    color: Color.fromRGBO(227, 227, 227, 1),
-                    padding: EdgeInsets.symmetric(horizontal: 30.0),
-                    onPressed: () => Navigator.pushNamed(context, 'my_studio',
-                        arguments: usuario),
-                  ),
-                ),
-                SizedBox(height: 30.0),
-                SizedBox(
-                  width: 150,
-                  height: 50,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: Text('Mercado',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: "Raleway",
-                            fontWeight: FontWeight.bold)),
-                    textColor: Color.fromRGBO(115, 115, 115, 1.0),
-                    color: Color.fromRGBO(227, 227, 227, 1),
-                    padding: EdgeInsets.symmetric(horizontal: 30.0),
-                    onPressed: () => Navigator.pushNamed(context, 'items_buy',
-                        arguments: usuario),
-                  ),
-                ),
-                SizedBox(height: 30.0),
-                SizedBox(
-                  width: 150,
-                  height: 50,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: Text('Servicios',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: "Raleway",
-                            fontWeight: FontWeight.bold)),
-                    textColor: Color.fromRGBO(115, 115, 115, 1.0),
-                    color: Color.fromRGBO(227, 227, 227, 1),
-                    padding: EdgeInsets.symmetric(horizontal: 30.0),
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'services',
-                          arguments: usuario);
-                    },
-                  ),
-                ),
+        margin: EdgeInsets.symmetric(vertical: 5.0),
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              _mostrarFotoPerfil(),
+              SizedBox(height: 20.0),
+              _carruselStudio(usuario),
+              SizedBox(height: 25.0),
+              _carruselMarket(usuario),
+              SizedBox(height: 25.0),
+              _carruselServicio(usuario),
 
-                /*  RaisedButton.icon(
+              /*  RaisedButton.icon(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(5.0),
                 ),
@@ -134,29 +73,30 @@ class _HomePageState extends State<HomePage> {
 
 
              */
-              ],
-            ),
+            ],
           ),
-
         ),
       ),
     );
   }
 
   Widget _mostrarFotoPerfil() {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(35.0)),
-          image: DecorationImage(
-            image: AssetImage('assets/img/APP ICON.png'),
-            fit: BoxFit.cover,
-          )),
-      height: 220.0,
-      width: 220.0,
+    return Padding(
+      padding: EdgeInsets.all(0.0),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(35.0)),
+            image: DecorationImage(
+              image: AssetImage('assets/img/APP ICON.png'),
+              fit: BoxFit.cover,
+            )),
+        height: 155.0,
+        width: 155.0,
+      ),
     );
   }
 
-  List<Widget> crear() {
+  /*List<Widget> crear() {
     return [
       RaisedButton.icon(
         shape: RoundedRectangleBorder(
@@ -185,5 +125,341 @@ class _HomePageState extends State<HomePage> {
         onPressed: () => Navigator.pushNamed(context, 'ver_producto'),
       ),
     ];
+  }
+
+
+   */
+  Widget _carruselStudio(UserModel usuario) {
+    return Stack(
+      children: [
+        Padding(
+            padding: EdgeInsets.only(),
+            child: Container(
+              color: Color.fromRGBO(227, 227, 227, 1),
+              height: MediaQuery.of(context).size.height - 570,
+              width: MediaQuery.of(context).size.width,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(
+                    width: 125,
+                  ),
+                  elementoMenu('Proyectos'),
+                  elementoMenu('Tienda'),
+                  elementoMenu('Contratos'),
+                  SizedBox(
+                    width: 15,
+                  ),
+                ],
+              ),
+            )),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+              padding: EdgeInsets.only(),
+              child: ClipPath(
+                  clipper: ClippCarrusel(),
+                  child: Container(
+                      color: Color.fromRGBO(112, 252, 118, 1),
+                      height: MediaQuery.of(context).size.height - 570,
+                      width: MediaQuery.of(context).size.width - 280,
+                      child: InkWell(
+                        onTap: () => Navigator.pushNamed(context, 'my_studio',
+                            arguments: usuario),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 10,
+                                right: 18,
+                              ),
+                              child: Container(
+                                  height:
+                                      MediaQuery.of(context).size.height - 610,
+                                  width:
+                                      MediaQuery.of(context).size.width - 335,
+                                  /*decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  border: Border.all(
+                                    width: 1.3,
+                                    color: Color.fromRGBO(0, 51, 51, 0.8),
+                                  ),
+                                ),
+
+                                 */
+                                  child: Icon(
+                                    Icons.local_convenience_store,
+                                    color: Colors.black,
+                                    size: 75,
+                                  )
+                                  /* ImageIcon(
+                                  AssetImage("assets/img/studioIcon.png"),
+                                  color: Colors.black,
+                                  size: 5,
+                                ),
+
+                                */
+
+                                  ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 0.0,
+                                right: 18,
+                              ),
+                              child: Text("Estudio",
+                                  style: TextStyle(
+                                      color: Color.fromRGBO(0, 51, 51, 0.8),
+                                      fontSize: 18,
+                                      fontFamily: "Raleway",
+                                      fontWeight: FontWeight.bold)),
+                            )
+                          ],
+                        ),
+                      )))),
+        )
+      ],
+    );
+  }
+
+  Widget _carruselMarket(UserModel usuario) {
+    return Stack(
+      children: [
+        Padding(
+            padding: EdgeInsets.only(),
+            child: Container(
+              color: Color.fromRGBO(227, 227, 227, 1),
+              height: MediaQuery.of(context).size.height - 570,
+              width: MediaQuery.of(context).size.width,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(
+                    width: 125,
+                  ),
+                  elementoMenu('Props'),
+                  elementoMenu('Guiones'),
+                  elementoMenu('Equipo'),
+                  elementoMenu('Accesorios'),
+                  SizedBox(
+                    width: 15,
+                  ),
+                ],
+              ),
+            )),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+              padding: EdgeInsets.only(),
+              child: ClipPath(
+                  clipper: ClippCarrusel(),
+                  child: Container(
+                      color: Color.fromRGBO(112, 252, 118, 1),
+                      height: MediaQuery.of(context).size.height - 570,
+                      width: MediaQuery.of(context).size.width - 280,
+                      child: InkWell(
+                        onTap: () => Navigator.pushNamed(context, 'items_buy',
+                            arguments: usuario),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 10,
+                                right: 18,
+                              ),
+                              child: Container(
+                                  height:
+                                      MediaQuery.of(context).size.height - 610,
+                                  width:
+                                      MediaQuery.of(context).size.width - 335,
+                                  /* decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  border: Border.all(
+                                    width: 1.3,
+                                    color: Color.fromRGBO(0, 51, 51, 0.8),
+                                  ),
+
+                                ),
+
+                                */
+                                  child: Icon(
+                                    Icons.store_mall_directory,
+                                    color: Colors.black,
+                                    size: 75,
+                                  )),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 0.0,
+                                right: 18,
+                              ),
+                              child: Text("Mercado",
+                                  style: TextStyle(
+                                      color: Color.fromRGBO(0, 51, 51, 0.8),
+                                      fontSize: 18,
+                                      fontFamily: "Raleway",
+                                      fontWeight: FontWeight.bold)),
+                            )
+                          ],
+                        ),
+                      )))),
+        )
+      ],
+    );
+  }
+
+  Widget _carruselServicio(UserModel usuario) {
+    return Stack(
+      children: [
+        Padding(
+            padding: EdgeInsets.only(),
+            child: Container(
+              color: Color.fromRGBO(227, 227, 227, 1),
+              height: MediaQuery.of(context).size.height - 570,
+              width: MediaQuery.of(context).size.width,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(
+                    width: 125,
+                  ),
+                  elementoMenu('Personal'),
+                  elementoMenu('Contratos'),
+                  elementoMenu('Espacios'),
+                  elementoMenu('Catering'),
+                  SizedBox(
+                    width: 15,
+                  ),
+                ],
+              ),
+            )),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+              padding: EdgeInsets.only(),
+              child: ClipPath(
+                  clipper: ClippCarrusel(),
+                  child: Container(
+                      color: Color.fromRGBO(112, 252, 118, 1),
+                      height: MediaQuery.of(context).size.height - 570,
+                      width: MediaQuery.of(context).size.width - 280,
+                      child: InkWell(
+                        onTap: () => Navigator.pushNamed(context, 'services',
+                            arguments: usuario),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 10,
+                                right: 18,
+                              ),
+                              child: Container(
+                                  height:
+                                      MediaQuery.of(context).size.height - 610,
+                                  width:
+                                      MediaQuery.of(context).size.width - 335,
+                                  /* decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  border: Border.all(
+                                    width: 1.3,
+                                    color: Color.fromRGBO(0, 51, 51, 0.8),
+                                  ),
+                                ),
+
+                                */
+                                  child: Icon(
+                                    Icons.work,
+                                    color: Colors.black,
+                                    size: 75,
+                                  )),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 0.0,
+                                right: 18,
+                              ),
+                              child: Text("Servicios",
+                                  style: TextStyle(
+                                      color: Color.fromRGBO(0, 51, 51, 0.8),
+                                      fontSize: 18,
+                                      fontFamily: "Raleway",
+                                      fontWeight: FontWeight.bold)),
+                            )
+                          ],
+                        ),
+                      )))),
+        )
+      ],
+    );
+  }
+
+  Widget elementoMenu(String name) {
+    return Padding(
+        padding: EdgeInsets.only(left: 5),
+        child: InkWell(
+            onTap: () {},
+            child: Container(
+              //color: Colors.amber,
+              constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height - 570,
+                  maxWidth: MediaQuery.of(context).size.width - 250,
+                  minWidth: MediaQuery.of(context).size.width - 310),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height - 615,
+                      width: MediaQuery.of(context).size.width - 340,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        border: Border.all(
+                          width: 1.3,
+                          color: Color.fromRGBO(0, 51, 51, 0.8),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 5),
+                    child: Text(name,
+                        style: TextStyle(
+                            color: Color.fromRGBO(0, 51, 51, 0.8),
+                            fontSize: 18,
+                            fontFamily: "Raleway",
+                            fontWeight: FontWeight.bold)),
+                  )
+                ],
+              ),
+            )));
+  }
+}
+
+class ClippCarrusel extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    // TODO: implement getClip
+    path.lineTo(size.width, 0.0);
+    path.lineTo((size.width / 4) * 3, size.height);
+
+    //path.lineTo(0, 0);
+    path.lineTo(0.0, size.height);
+    //path.moveTo((size.width/3)*2,size.width);
+    //path.lineTo(size.width, size.height);
+    //path.lineTo(0.0, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return false;
   }
 }
