8a31029be5041ae23a4191f66483556fff7664c9
sebastianpedraza2 <46971682+sebastianpedraza2@users.noreply.github.com>
2020-10-01 19:16:06 -0500

    Cambio Project details

--- Clapp/lib/main.dart
+++ Clapp/lib/main.dart
@@ -18,14 +18,14 @@ class MyApp extends StatelessWidget {
     return Provider(
         child: MaterialApp(
 
-         /* builder: (context, widget) => ResponsiveWrapper.builder(
+        /* builder: (context, widget) => ResponsiveWrapper.builder(
 
               BouncingScrollWrapper.builder(context, widget),
               maxWidth: 1200,
-              minWidth: 480,
+              minWidth: 450,
               defaultScale: true,
               breakpoints: [
-                ResponsiveBreakpoint.resize(480, name: MOBILE),
+                ResponsiveBreakpoint.resize(450, name: MOBILE),
                 ResponsiveBreakpoint.autoScale(800, name: TABLET),
                 ResponsiveBreakpoint.resize(1000, name: DESKTOP),
                 ResponsiveBreakpoint.autoScale(2460, name: '4K'),
@@ -34,7 +34,9 @@ class MyApp extends StatelessWidget {
               //background: Container(color: Color(0xFFF5F5F5))
           ),
 
-          */
+         */
+
+
 
 
 
